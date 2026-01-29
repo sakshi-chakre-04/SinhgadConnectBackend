3329b609cfc6624e4948f8a62431e2349363ad74
sakshi-chakre-04 <sakshichakre0421@gmail.com>
2026-01-29 16:53:57 +0530

    p

--- server.js
+++ server.js
@@ -34,6 +34,8 @@ app.use('/api/', limiter);
 // CORS setup
 const allowedOrigins = [
   'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:5175',
   'http://localhost:3000',
   'https://sinhgad-connect-frontend.vercel.app',
   /\.vercel\.app$/  // Allow all Vercel preview deployments
